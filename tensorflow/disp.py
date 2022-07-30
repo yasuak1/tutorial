dab3d75413320d3eea6931113d3d242d60713bd4
yasuak1 <yasuakimizuno36@gmail.com>
2022-07-30 19:29:52 +0900

    add l2_model

--- tensorflow/disp.py
+++ tensorflow/disp.py
@@ -20,33 +20,16 @@ def disp_acc(result, metrics):
         plt.legend()
     plt.show()
 
-def plot_image(i, predictions_array, true_label, class_names, img):
-    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16,10))
 
-    plt.imshow(img, cmap=plt.cm.binary)
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_'+key], '--', label=name.title()+' Val')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(), label=name.title()+' Train')
+                
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_',' ').title())
+    plt.legend()
 
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label: color = 'blue'
-    else: color='red'
-
-    plt.xlabel("{} {:2.0f}% ({})".format(
-        class_names[predicted_label],
-        100*np.max(predictions_array),
-        class_names[true_label]),
-        color=color
-        )
-
-def plot_value_array(i, predictions_array, true_label):
-    predictions_array, true_label = predictions_array[i], true_label[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
+    plt.xlim([0,max(history.epoch)])
+    plt.show()
--- tensorflow/train_review_using_l2_model.py
+++ tensorflow/train_review_using_l2_model.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from tensorflow import keras
-
 import numpy as np
 import matplotlib.pylab as plt
 from disp import plot_history
@@ -8,7 +7,7 @@ from disp import plot_history
 def main():
     NUM_WORDS = 10000
     (train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
-    
+
     def multi_hot_sequences(sequences, dimension):
         results = np.zeros((len(sequences), dimension))
         for i, word_indices in enumerate(sequences):
@@ -17,9 +16,6 @@ def main():
     
     train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
     test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
-
-    #plt.plot(train_data[0])
-    #plt.show()
 
     # base
     baseline_model = keras.Sequential([
@@ -44,42 +40,20 @@ def main():
         verbose=2
     )
 
-
-    # smaller
-    smaller_model = keras.Sequential([
-        keras.layers.Dense(4, activation='relu', input_shape=(NUM_WORDS,)),
-        keras.layers.Dense(4, activation='relu'), 
+    # l2_model
+    l2_model = keras.models.Sequential([
+        keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001), activation='relu', input_shape=(NUM_WORDS,)),
+        keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001), activation='relu'),
         keras.layers.Dense(1, activation='sigmoid')
-    ])
-    smaller_model.compile(
+        ])
+    l2_model.compile(
         optimizer='adam',
         loss='binary_crossentropy',
         metrics=['accuracy', 'binary_crossentropy']
     )
-    smaller_model.summary()
+    l2_model.summary()
 
-    smaller_history = smaller_model.fit(
-        train_data, train_labels,
-        epochs=20,
-        batch_size=512,
-        validation_data=(test_data, test_labels),
-        verbose=2
-    )
-
-    # bigger
-    bigger_model = keras.Sequential([
-        keras.layers.Dense(512, activation='relu', input_shape=(NUM_WORDS,)),
-        keras.layers.Dense(512, activation='relu'),
-        keras.layers.Dense(1, activation='sigmoid')
-    ])
-    bigger_model.compile(
-        optimizer='adam',
-        loss='binary_crossentropy',
-        metrics=['accuracy', 'binary_crossentropy']
-    )
-    bigger_model.summary()
-
-    bigger_history = bigger_model.fit(
+    l2_model_history = l2_model.fit(
         train_data, train_labels,
         epochs=20,
         batch_size=512,
@@ -88,12 +62,11 @@ def main():
     )
 
     plot_history([
-        ('baseline', baseline_history),
-        ('smaller', smaller_history),
-        ('bigger', bigger_history),
+        ('baseline', baseline_history)
+        ('l2', l2_model_history)
     ])
 
-
+    
 
 if __name__ == '__main__':
     main()
