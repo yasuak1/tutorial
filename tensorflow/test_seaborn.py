780f5b2aeb972f90eb73a92b150853148f3e82ad
yasuak1 <yasuakimizuno36@gmail.com>
2022-07-30 18:49:12 +0900

    add overfit

--- tensorflow/test_seaborn.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#df = pd.read_csv("iris.csv")
-df = sns.load_dataset("iris")  #手元にiris.csvがない場合
-
-sns.distplot(df.sepal_length,kde = True)
-plt.show()
